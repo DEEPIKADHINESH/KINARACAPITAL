c5e70fe97d992c70e57990f8ea60191fc9bc9e8a
Deepika.A <adeepika78@gmail.com>
2023-05-29 12:29:40 +0530

    database info

--- backend/details-models.js
+++ backend/details-models.js
@@ -11,15 +11,45 @@ const detailsSchema= new mongoose.Schema({
     }
 })
 const Details=mongoose.model("Details",detailsSchema)
-// async function createDetails(){
-//     const detail=new Details({
-//         name:"person6",
-//         id:20,
-//         totalMarks:77
-//     })
+async function createDetails(){
+    const detail1=new Details({
+        name:"person1",
+        id:1,
+        totalMarks:77
+    })
+    const detail2=new Details({
+        name:"person2",
+        id:2,
+        totalMarks:78
+    })
+    const detail3=new Details({
+        name:"person3",
+        id:3,
+        totalMarks:79
+    })
+    const detail4=new Details({
+        name:"person4",
+        id:4,
+        totalMarks:85
+    })
+    const detail5=new Details({
+        name:"person5",
+        id:5,
+        totalMarks:78
+    })
+    const detail6=new Details({
+        name:"person6",
+        id:6,
+        totalMarks:100
+    })
    
-//     const result=await  detail.save()
-//     console.log(result)
-// }
-// createDetails()
+    const result1=await  detail1.save()
+    const result2=await  detail2.save()
+    const result3=await  detail3.save()
+    const result4=await  detail4.save()
+    const result5=await  detail5.save()
+    const result6=await  detail6.save()
+    console.log(result1,result2,result3,result4,result5,result6)
+}
+createDetails()
 exports.Details=Details
